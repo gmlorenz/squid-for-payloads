0ab3d61660147d5c0d8666d7903a6c8743f9aa92
gmlorenz <35004437+gmlorenz@users.noreply.github.com>
2019-09-05 02:25:49 +0400

    Create squid.sh

---
+++ squid.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+MYIP=$(wget -qO- ipv4.icanhazip.com); 
+MYIP2="s/xxxxxxxxx/$MYIP/g";
+# go to root
+cd
+# disable ipv6
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6 
+sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
+#export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games";
+
+apt remove -y squid;
+apt-get -y install squid
+# install squid
+cd 
+wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/PHCarl10/Deb7AutoScriptVPS/master/squid3.conf" 
+
+sed -i $MYIP2 /etc/squid/squid.conf; 
+service squid restart 
+clear;
+echo -e "\e[94m Squid AutoScript by Renz " 
+echo -e "\e[94m installed [OK] "
